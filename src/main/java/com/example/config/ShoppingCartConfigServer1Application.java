fe9c198860382210409bcf5eb45bf04cd17368b1
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2026-02-16 18:54:14 +0530

    first commit for payment service

---
+++ src/main/java/com/example/config/ShoppingCartConfigServer1Application.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartConfigServer1Application.class, args);
+	}
+
+}
